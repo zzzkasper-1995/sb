37204d9f01c8272ae8d1651593bb35b17f5513a4
a.mazur <a.mazur@sberauto.com>
2021-02-12 15:41:32 +0300

    init

---
+++ SberId.h
@@ -0,0 +1,13 @@
+//
+//  SberId.h
+//  SberId
+//
+//  Created by Mazur Aleksey on 03.02.2021.
+//  Copyright © 2021 Facebook. All rights reserved.
+//
+
+#ifndef SberId_h
+#define SberId_h
+
+
+#endif /* SberId_h */
